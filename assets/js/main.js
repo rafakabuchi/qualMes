8f4c1fa8e0a55cf20de67648a3008ef607534c4d
Rafa Kabuchi <rkabuchi@gmail.com>
2021-03-07 20:59:28 -0300

    exercicio de if else, com meses

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+qualMes = prompt("Digite um número para saber o mês")
+
+if (qualMes == 1) {
+    meses.innerHTML = "Você digitou o mês de Janeiro"
+}
+if (qualMes == 2) {
+    meses.innerHTML = "Você digitou o mês de Fevereiro"
+}
+if (qualMes == 3) {
+    meses.innerHTML = " Você digitou o mês de Março"
+}
+if (qualMes == 4) {
+    meses.innerHTML = "Você digitou o mês de Abril"
+}
+if (qualMes == 5) {
+    meses.innerHTML = "Você digitou o mês de Maio"
+}
+if (qualMes == 6) {
+    meses.innerHTML = "Você digitou o mês de Junho"
+}
+if (qualMes == 7) {
+    meses.innerHTML = "Você digitou o mês de Julho"
+}
+if (qualMes == 8) {
+    meses.innerHTML = "Você digitou o mês de Agosto"
+}
+if (qualMes == 9) {
+    meses.innerHTML = "Você digitou o mês de Setembro"
+}
+if (qualMes == 10) {
+    meses.innerHTML = "Você digitou o mês de Outubro"
+}
+if (qualMes == 11) {
+    meses.innerHTML = "Você digitou o mês de Novembro"
+}
+if (qualMes == 12) {
+    meses.innerHTML = "Você digitou o mês de Dezembro"
+} else {
+    meses.innerHTML = "Você digitou um mês incorreto"
+}
