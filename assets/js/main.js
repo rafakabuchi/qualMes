56a653d3235c8c3ffb7a6267bc1504c11a933358
Rafa Kabuchi <rkabuchi@gmail.com>
2021-03-07 21:28:48 -0300

    ajustes da condicao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,44 @@
-qualMes = prompt("Digite um número para saber o mês")
+const qualMes = prompt("Digite um número para saber o mês")
+
+const meses = document.querySelector('#meses')
 
 if (qualMes == 1) {
     meses.innerHTML = "Você digitou o mês de Janeiro"
 }
-if (qualMes == 2) {
+else if (qualMes == 2) {
     meses.innerHTML = "Você digitou o mês de Fevereiro"
 }
-if (qualMes == 3) {
+else if (qualMes == 3) {
     meses.innerHTML = " Você digitou o mês de Março"
 }
-if (qualMes == 4) {
+else if (qualMes == 4) {
     meses.innerHTML = "Você digitou o mês de Abril"
 }
-if (qualMes == 5) {
+else if (qualMes == 5) {
     meses.innerHTML = "Você digitou o mês de Maio"
 }
-if (qualMes == 6) {
+else if (qualMes == 6) {
     meses.innerHTML = "Você digitou o mês de Junho"
 }
-if (qualMes == 7) {
+else if (qualMes == 7) {
     meses.innerHTML = "Você digitou o mês de Julho"
 }
-if (qualMes == 8) {
+else if (qualMes == 8) {
     meses.innerHTML = "Você digitou o mês de Agosto"
 }
-if (qualMes == 9) {
+else if (qualMes == 9) {
     meses.innerHTML = "Você digitou o mês de Setembro"
 }
-if (qualMes == 10) {
+else if (qualMes == 10) {
     meses.innerHTML = "Você digitou o mês de Outubro"
 }
-if (qualMes == 11) {
+else if (qualMes == 11) {
     meses.innerHTML = "Você digitou o mês de Novembro"
 }
-if (qualMes == 12) {
+else if (qualMes == 12) {
     meses.innerHTML = "Você digitou o mês de Dezembro"
 } else {
     meses.innerHTML = "Você digitou um mês incorreto"
 }
+
+console.log(qualMes.typeof);
